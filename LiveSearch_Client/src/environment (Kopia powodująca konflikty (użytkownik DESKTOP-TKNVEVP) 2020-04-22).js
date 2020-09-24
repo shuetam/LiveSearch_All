240f1bdcdcaec3b5c3c2dccbcf3cc143165b1553
mateusz <mateuszbieda.mb@gmail.com>
2020-09-24 22:23:12 +0200

    update

---
+++ LiveSearch_Client/src/environment (Kopia powodująca konflikty (użytkownik DESKTOP-TKNVEVP) 2020-04-22).js
@@ -0,0 +1,65 @@
+//export const API = "http://localhost:5000/api/"
+
+export const URL = {
+
+    api: "http://localhost:53072/api/", 
+    //debugger//api: "http://localhost:5000/api/", 
+
+    //api: "https://www.fitize.pl/api/", 
+
+    login: "SocialUser/user",
+    radioSongs: "song/allradiosongs/",
+    randomSongs: "song/radiorandom",
+    //tvAdminMovies: "adminmovie/takemovies",
+    //radioAdminSongs: "adminsong/allradiosongs/",
+    tvMovies: "movie/takemovies",
+    userFolders: "UserDesktop/getfolders",
+    addToFolder: "UserDesktop/addtofolder",
+    createFolder: "UserDesktop/createfolder",
+    saveLocations: "UserDesktop/savelocations",
+    userIconsIds: "UserDesktop/geticonsid",
+    removeIcon: "UserDesktop/removeentity",
+    addIcon: "UserDesktop/addicon",
+    userIcons: "UserDesktop/geticons",
+    userImages: "UserDesktop/getimages",
+    moveIcon: "UserDesktop/movefromfolder",
+    findIcons: "UserDesktop/findiconsfromurl",
+    bestsellers: "bestsellers/getbestsellers",
+    changeTitle: "UserDesktop/changetitle",
+    userSpotify: "UserDesktop/getspotify",
+    editYoutube: "admin/edityoutube",
+    changeLocation: "admin/changeyoutubelocation",
+    deleteYoutube: "admin/deleteyoutube",
+    getallerrors: "admin/getallerrors",
+    getallusers: "admin/getallusers",
+    getUpdateInfo: "admin/getupdatesinfo",
+    startSongs: "admin/startsongsupdating",
+    startMovies: "admin/starttvmoviesupdating",
+    startMoviesWYT: "admin/starttvmoviesupdatingwithoutyt",
+    getMoviesForAdmin: "admin/getallmoviesinfuture",
+    changeUserStatus : "admin/changeuserstatus",
+    changeBook: "admin/changebookproperties",
+    startBooks:  "admin/startbooksupdating",
+    changeMovieRating: "admin/changemovierating",
+    update: "updating/updates"
+    
+}
+
+export const PATHES = {
+    songs: '/muzyka_radio',
+    songsAdmin: '/songsadmin',
+    movies: '/filmy_tv',
+    moviesAdmin: '/moviesadmin',
+    adminPanel: '/adminpanel',
+    userPulpit: '/profil/pulpit',
+    bestsellers: '/literatura_bestsellery',
+    policy: '/polityka-prywatnosci',
+    contact: '/kontakt',
+    information: '/informacje',
+    userFolder: '/profil/folder/',
+    bestsellersAdmin: "/bestsellersadmin"
+}
+
+
+
+
