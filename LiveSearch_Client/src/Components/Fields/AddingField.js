46409a7428298079cf50ed45665ae45c5c294523
Mateusz Bieda <mateusz_b@me.com>
2021-02-02 00:31:01 +0100

    another is coing

--- LiveSearch_Client/src/Components/Fields/AddingField.js
+++ LiveSearch_Client/src/Components/Fields/AddingField.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Field.css';
 import { Route, NavLink, Switch, withRouter } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 
 class AddingField extends Component {
@@ -32,6 +33,34 @@ class AddingField extends Component {
 
 
     render() {
+
+let spotify = <div className="imageField">              
+<iframe src={this.props.id}  width="300" height="315" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+
+let video =    <div className="field">
+<ReactPlayer
+url={this.props.play.includes("www.")? this.props.play : "https://www.youtube.com/watch?v=" + this.props.play}
+height= '315px'
+width= '560px'
+playing ={this.state.playMain} 
+controls = {true}
+onReady={this.mainVideoOnReady}
+onPlay={this.playReflect}
+onPause={this.pauseReflect}
+/* onSeek={this.onChange} */
+onEnded={this.props.nextSong}
+/>  
+</div>
+
+let image =  <div className="imageField" >
+<img 
+   class="imgField"
+  src={this.props.src}></img>  
+</div>
+
+
+
  
         let iconIcons = <div style={{fontSize: "17px"}}><i class="icon-youtube"/>
         <i class="icon-spotify"/>
